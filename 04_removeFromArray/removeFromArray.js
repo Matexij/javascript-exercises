65c97ab04b93dac992dc7bde66c144fafb49fda3
Matexij <162997577+Matexij@users.noreply.github.com>
2024-03-14 15:28:26 +0100

    passed 7 exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(input, ...params) {
+    let result = input
+    params.forEach((el)=>{
+        result = result.filter(function(item){
+            return item !== el
+        })
+    })
+    return result
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if(start < 0 || end < 0)
+        return 'ERROR'
+    if(typeof start !== "number" || typeof end !== "number")
+        return 'ERROR'
+    let sum = 0;
+    const minNum = Math.min(start, end)
+    const maxNum = Math.max(start, end)
+    for(let i=minNum; i<=maxNum; i++){
+        sum +=i
+    }
+    return sum
 };
 
 // Do not edit below this line
