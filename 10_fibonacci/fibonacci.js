ac3ace7ac887f00f788cbad2a54f8a8846831ccc
Matexij <162997577+Matexij@users.noreply.github.com>
2024-03-26 14:27:18 +0100

    completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    index = Number(index)
+    if(index<=2 && index>0)
+        return 1
+    if(index == 0)
+        return 0
+    if(index<0)
+        return 'OOPS'
+    const sum = fibonacci(index-1)+fibonacci(index-2)
+    return sum
 };
 
 // Do not edit below this line
