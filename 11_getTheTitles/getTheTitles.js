9ba99a1384f8a661119ade0559e43a1912162a3a
Matexij <162997577+Matexij@users.noreply.github.com>
2024-03-26 14:36:13 +0100

    completed simplier

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,7 +1,5 @@
 const getTheTitles = function(books) {
-    const bookList = []
-    books.forEach((book) => bookList.push(book.title))
-    return bookList
+    return books.map(book => book.title)
 };
 
 // Do not edit below this line
