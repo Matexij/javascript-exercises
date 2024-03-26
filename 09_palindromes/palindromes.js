8748f691173705b3963c0699d8ed2990689202e8
Matexij <162997577+Matexij@users.noreply.github.com>
2024-03-26 13:37:30 +0100

    completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,10 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    const abc = 'abcdefghijklmnopqrstuvwxyz0123456789'
+
+    const newS = s.toLowerCase().split('').filter((letter) => abc.includes(letter)).join('')
+    const reversedS = newS.split('').reverse().join('')
+
+    return newS === reversedS
 
 };
 
