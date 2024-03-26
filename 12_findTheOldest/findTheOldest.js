ba533156226423db04d5279b0ac9517d381689b3
Matexij <162997577+Matexij@users.noreply.github.com>
2024-03-26 15:01:08 +0100

    completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const now = (new Date).getFullYear()
+    people.sort((a,b) => ((b.yearOfDeath === undefined ? now : b.yearOfDeath)-b.yearOfBirth) - ((a.yearOfDeath === undefined ? now : a.yearOfDeath)-a.yearOfBirth))
+    return people[0]
 };
 
 // Do not edit below this line
