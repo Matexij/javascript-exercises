0a588f87981924b1a5cc3ec59a8daf12defcc244
Matexij <162997577+Matexij@users.noreply.github.com>
2024-03-26 14:35:33 +0100

    completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const bookList = []
+    books.forEach((book) => bookList.push(book.title))
+    return bookList
 };
 
 // Do not edit below this line
